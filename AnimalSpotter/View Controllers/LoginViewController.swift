87009013b69bc6371718b839a329e2bd1527e1a9
Ben Gohlke <jcgohlke@me.com>
2020-02-12 11:56:18 -0500

    Initial commit of starter project.

---
+++ AnimalSpotter/View Controllers/LoginViewController.swift
@@ -0,0 +1,43 @@
+//
+//  LoginViewController.swift
+//  AnimalSpotter
+//
+//  Created by Ben Gohlke on 4/16/19.
+//  Copyright © 2019 Lambda School. All rights reserved.
+//
+
+import UIKit
+
+enum LoginType {
+    case signUp
+    case signIn
+}
+
+class LoginViewController: UIViewController, UITextFieldDelegate {
+    
+    @IBOutlet private weak var usernameTextField: UITextField!
+    @IBOutlet private weak var passwordTextField: UITextField!
+    @IBOutlet private weak var loginTypeSegmentedControl: UISegmentedControl!
+    @IBOutlet private weak var signInButton: UIButton!
+    
+    var apiController: APIController?
+    var loginType = LoginType.signUp
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        signInButton.backgroundColor = UIColor(hue: 190/360, saturation: 70/100, brightness: 80/100, alpha: 1.0)
+            signInButton.tintColor = .white
+            signInButton.layer.cornerRadius = 8.0
+    }
+    
+    // MARK: - Action Handlers
+    
+    @IBAction func buttonTapped(_ sender: UIButton) {
+        // perform login or sign up operation based on loginType
+    }
+    
+    @IBAction func signInTypeChanged(_ sender: UISegmentedControl) {
+        // switch UI between login types
+    }
+}
---
+++ README.md
@@ -0,0 +1,5 @@
+# iOS Guided Project Starter: Animal Spotter Part I
+
+This is a starter project for the module _API Authentication with Account Signup and Login_ in the sprint _iOS Networking Basics_.
+
+Begin by forking it to your own GitHub account, then cloning it to your local machine.
